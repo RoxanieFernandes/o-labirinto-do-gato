379c6ea91e3108a303b80ca664fd9d61850dd693
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2023-09-15 12:34:25 -0300

    Altera index.js e Atualiza a documentação do README com instruções de instalação

---
+++ README.md
@@ -0,0 +1,35 @@
+# Labirinto do Gato
+
+Bem-vindo ao Labirinto do Gato, um jogo simples em HTML5 Canvas onde você ajuda um gato a escapar de um labirinto antes que ele fique com fome!
+
+## Como Jogar
+
+1. Abra o arquivo `index.html` no seu navegador ou hospede o jogo em um servidor web.
+
+2. Clique no botão "Iniciar Jogo" para começar. O temporizador começará a contar regressivamente a partir de 1 minuto.
+
+3. Use as teclas de seta (Seta para a Esquerda, Seta para a Direita, Seta para Cima e Seta para Baixo) para mover o gato pelo labirinto.
+
+4. Se você conseguir levar o gato até a saída do labirinto antes que o tempo acabe, você vence o jogo. Caso contrário, o gato ficará com fome, e o jogo terminará.
+
+5. Se você quiser jogar novamente, basta clicar no botão "Iniciar Jogo" novamente para reiniciar o jogo e o temporizador.
+
+## Controles
+
+- Seta para a Esquerda: Move o gato para a esquerda.
+- Seta para a Direita: Move o gato para a direita.
+- Seta para Cima: Move o gato para cima.
+- Seta para Baixo: Move o gato para baixo.
+
+## Tecnologias Usadas
+
+- HTML5 Canvas: Para renderizar o jogo.
+- JavaScript: Para a lógica do jogo.
+- CSS: Para o estilo e layout.
+
+## Autor
+
+Este jogo foi desenvolvido por Roxanie Fernandes.
+
+
+Divirta-se jogando o Labirinto do Gato!
--- js/index.js
+++ js/index.js
@@ -1,233 +1,259 @@
-console.log("JS connected");
-const startBtn = document.querySelector("#start-btn");
-var mostrador = document.querySelector('#mostrador');
+document.addEventListener("DOMContentLoaded", function () {
+  const startBtn = document.querySelector("#start-btn");
+  const mostrador = document.querySelector('#mostrador');
 
-(function () {
-  var cnv = document.querySelector("canvas");
-  var ctx = cnv.getContext("2d");
-  var height = cnv.height,
-    width = cnv.width;
-  var left = 37,
-    up = 38,
-    right = 39,
-    down = 40;
-  var mvLeft = (mvUp = mvRight = mvDown = false);
-  const walls = [];
-
-  //tamanho dos blocos
-  var tileSize = 22;
-
-  // images\catfaceA.png
-
-  var img = new Image();
-      img.src = "./images/cat.png";
-      img.addEventListener("load",function(){
-        requestAnimationFrame(loop);
-      },false);
-
-  //player
-  var player = {
-    x: tileSize - 20,
-    y: tileSize + 70,
-    width: 32,
-    height: 32,
-    speed: 2,
-    srcX: 0,
-    srcY:0,
-  };
-
-  //mapa do labirinto
-  var maze = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-    [1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  ];
-
-  for (var row in maze) {
-    for (var column in maze[row]) {
-      var tile = maze[row][column];
-
-      if (tile === 1) {
-        var wall = {
-          x: tileSize * column,
-          y: tileSize * row,
-          width: tileSize,
-          height: tileSize,
-        };
-
-        walls.push(wall);
-      }
-    }
-  }
-
-  function blockRectangle(objA, objB) {
-    var distX = objA.x + objA.width / 2 - (objB.x + objB.width / 2);
-    var distY = objA.y + objA.height / 2 - (objB.y + objB.height / 2);
-
-    var sumWidth = (objA.width + objB.width) / 2;
-    var sumHeight = (objA.height + objB.height) / 2;
-
-    if (Math.abs(distX) < sumWidth && Math.abs(distY) < sumHeight) {
-      var overlapX = sumWidth - Math.abs(distX);
-      var overlapY = sumHeight - Math.abs(distY);
-
-      if (overlapX > overlapY) {
-        objA.y = distY > 0 ? objA.y + overlapY : objA.y - overlapY;
-      } else {
-        objA.x = distX > 0 ? objA.x + overlapX : objA.x - overlapX;
-      }
-    }
-  }
-
-  window.addEventListener("keydown", keydownHandler, false);
-  window.addEventListener("keyup", keyupHandler, false);
-
-  function keydownHandler(e) {
-    var key = e.keyCode;
-    switch (key) {
-      case left:
-        mvLeft = true;
-        break;
-      case right:
-        mvRight = true;
-        break;
-      case up:
-        mvUp = true;
-        break;
-      case down:
-        mvDown = true;
-        break;
-    }
-  }
-
-  function keyupHandler(e) {
-    var key = e.keyCode;
-    switch (key) {
-      case left:
-        mvLeft = false;
-        break;
-      case right:
-        mvRight = false;
-        break;
-      case up:
-        mvUp = false;
-        break;
-      case down:
+  (function () {
+    const cnv = document.querySelector("canvas");
+    const ctx = cnv.getContext("2d");
+    const height = cnv.height,
+          width = cnv.width;
+    const left = 37,
+          up = 38,
+          right = 39,
+          down = 40;
+    let mvLeft = false,
+        mvUp = false,
+        mvRight = false,
         mvDown = false;
-        break;
-    }
-  }
+    const walls = [];
+    let gameStarted = false;
 
-  function update() {
-    //movimento do player
-    if (mvLeft && !mvRight) {
-      player.x -= player.speed;
-    } else if (mvRight && !mvLeft) {
-      player.x += player.speed;
-    }
-    if (mvUp && !mvDown) {
-      player.y -= player.speed;
-    } else if (mvDown && !mvUp) {
-      player.y += player.speed;
-    }
-    for (var i in walls) {
-      var wall = walls[i];
-      blockRectangle(player, wall);
-    }
-  }
 
-  // renderização
-  function render() {
-    ctx.clearRect(0, 0, width, height);
-  
-    for (var row in maze) {
-      for (var column in maze) {
-        var tile = maze[row][column];
+    //tamanho dos blocos
+    const tileSize = 22;
+
+    const img = new Image();
+        img.src = "./images/cat.png";
+        img.addEventListener("load",function(){
+          requestAnimationFrame(loop);
+        },false);
+
+    //player
+    const player = {
+      x: tileSize - 20,
+      y: tileSize + 70,
+      width: 32,
+      height: 32,
+      speed: 2,
+      srcX: 0,
+      srcY:0,
+    };
+
+    //mapa do labirinto
+    const maze = [
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1],
+      [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+      [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+      [1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ];
+
+  cnv.width = maze[0].length * tileSize;
+  cnv.height = maze.length * tileSize;
+
+    for (const row in maze) {
+      for (const column in maze[row]) {
+        const tile = maze[row][column];
+
         if (tile === 1) {
-          var x = column * tileSize;
-          var y = row * tileSize;
-          ctx.fillRect(x, y, tileSize, tileSize);
+          const wall = {
+            x: tileSize * column,
+            y: tileSize * row,
+            width: tileSize,
+            height: tileSize,
+          };
+
+          walls.push(wall);
         }
       }
     }
-    ctx.fillStyle = "#024059";
-    ctx.drawImage(
-      img,
-      player.srcX, player.srcY, player.width, player.height,
-      player.x, player.y, player.width, player.height,
-    )
-   
-    ctx.restore();
-  }
-// timer contador
-  startBtn.addEventListener('click', function() {
-    new Timer(1, mostrador, function() {
+
+    function blockRectangle(objA, objB) {
+      const distX = objA.x + objA.width / 2 - (objB.x + objB.width / 2);
+      const distY = objA.y + objA.height / 2 - (objB.y + objB.height / 2);
+
+      const sumWidth = (objA.width + objB.width) / 2;
+      const sumHeight = (objA.height + objB.height) / 2;
+
+      if (Math.abs(distX) < sumWidth && Math.abs(distY) < sumHeight) {
+        const overlapX = sumWidth - Math.abs(distX);
+        const overlapY = sumHeight - Math.abs(distY);
+
+        if (overlapX > overlapY) {
+          objA.y = distY > 0 ? objA.y + overlapY : objA.y - overlapY;
+        } else {
+          objA.x = distX > 0 ? objA.x + overlapX : objA.x - overlapX;
+        }
+      }
+    }
+
+    window.addEventListener("keydown", keydownHandler, false);
+    window.addEventListener("keyup", keyupHandler, false);
+
+    function keydownHandler(e) {
+      const key = e.keyCode;
+      switch (key) {
+        case left:
+          mvLeft = true;
+          break;
+        case right:
+          mvRight = true;
+          break;
+        case up:
+          mvUp = true;
+          break;
+        case down:
+          mvDown = true;
+          break;
+      }
+    }
+
+    let playerWon = false;
+
+    function reiniciarJogo() {
+      player.x = tileSize - 20;
+      player.y = tileSize + 70;
+      playerWon = false;
+      gameStarted = false; // Reset do estado do jogo
+    }
+
+
+    function keyupHandler(e) {
+      const key = e.keyCode;
+      switch (key) {
+        case left:
+          mvLeft = false;
+          break;
+        case right:
+          mvRight = false;
+          break;
+        case up:
+          mvUp = false;
+          break;
+        case down:
+          mvDown = false;
+          break;
+      }
+    }
+
+
+    function update() {
+      //movimento do player
+      if (mvLeft && !mvRight) {
+        player.x -= player.speed;
+      } else if (mvRight && !mvLeft) {
+        player.x += player.speed;
+      }
+      if (mvUp && !mvDown) {
+        player.y -= player.speed;
+      } else if (mvDown && !mvUp) {
+        player.y += player.speed;
+      }
+      for (const i in walls) {
+        const wall = walls[i];
+        blockRectangle(player, wall);
+      }
+      if (player.x >= (maze[0].length - 1) * tileSize) {
+        playerWon = true;
+      }
+    }
+    
+    // renderização
+    function render() {
+      ctx.clearRect(0, 0, width, height);
+    
+      for (const row in maze) {
+        for (const column in maze) {
+          const tile = maze[row][column];
+          if (tile === 1) {
+            const x = column * tileSize;
+            const y = row * tileSize;
+            ctx.fillRect(x, y, tileSize, tileSize);
+          }
+        }
+      }
+      ctx.fillStyle = "#024059";
+      ctx.drawImage(
+        img,
+        player.srcX, player.srcY, player.width, player.height,
+        player.x, player.y, player.width, player.height,
+      )
+    }
+  // timer contador
+  startBtn.addEventListener('click', function () {
+    if (!gameStarted) {
+      new Timer(1, mostrador, function () {
         alert('Game Over! The cat is hungry!');
-    }).start();
+        reiniciarJogo();
+      }).start();
+      gameStarted = true; // Inicia o temporizador apenas quando o jogo começa
+    }
+  });
+
+  function Timer(mins, target, cb) {
+      this.counter = mins * 30;
+      this.target = target;
+      this.callback = cb;
+    }
+
+  Timer.prototype.pad = function(s) {
+      return (s < 10) ? '0' + s : s;
+  }
+  Timer.prototype.start = function(s) {
+      this.count();
+  }
+  Timer.prototype.done = function(s) {
+      if (this.callback) this.callback();
+  }
+  Timer.prototype.display = function(s) {
+      this.target.innerHTML = this.pad(s);
+  }
+  Timer.prototype.count = function(s) {
+      const self = this;
+      self.display.call(self, self.counter);
+      self.counter--;
+      const clock = setInterval(function() {
+          self.display(self.counter);
+          self.counter--;
+          if (self.counter < 0) {
+              clearInterval(clock);
+              self.done.call(self);
+          }
+      }, 1000);
+  }
+  function loop() {
+      update();
+      render();
+      if (playerWon) {
+        alert('Você ganhou! O gato conseguiu sair do labirinto a tempo!');
+        reiniciarJogo();
+      } else {
+        requestAnimationFrame(loop);
+      }
+    }
+
+  })();
 });
 
-function Timer(mins, target, cb) {
-    this.counter = mins * 45;
-    this.target = target;
-    this.callback = cb;
-}
-Timer.prototype.pad = function(s) {
-    return (s < 10) ? '0' + s : s;
-}
-Timer.prototype.start = function(s) {
-    this.count();
-}
-Timer.prototype.stop = function(s) {
-    this.count();
-}
-Timer.prototype.done = function(s) {
-    if (this.callback) this.callback();
-}
-Timer.prototype.display = function(s) {
-    this.target.innerHTML = this.pad(s);
-}
-Timer.prototype.count = function(s) {
-    var self = this;
-    self.display.call(self, self.counter);
-    self.counter--;
-    var clock = setInterval(function() {
-        self.display(self.counter);
-        self.counter--;
-        if (self.counter < 0) {
-            clearInterval(clock);
-            self.done.call(self);
-        }
-    }, 1000);
-}
-function loop() {
-  update();
-  render();
-  requestAnimationFrame(loop);
-}
-
-
-})();
